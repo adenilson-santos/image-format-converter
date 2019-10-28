58ceac06f48d5ccbe098f63f8a1e51274d498ffe
Adenilson Araújo dos Santos <adenilsonblog@gmail.com>
2019-10-27 21:53:20 -0300

    change to use rmagick

--- go_versa.rb
+++ go_versa.rb
@@ -1,17 +1,41 @@
+require 'rmagick'
+require 'byebug'
+
 def get_files
   puts "Initializing..."
-  files = Dir.glob("go_versa_png/*.png")
+  puts "Convertion setup -> RGB(250,250,250) HEX(#FAFAFA) CMKY(0,0,0,2)"
+  print "Insert a folder: "
+  folder = gets.chomp
+  puts ""
 
-  folder = Dir.glob("go_versa_tiff")
-  system("mkdir go_versa_tiff") if folder.length < 1
+  files = Dir.glob("#{folder}/*.png")
+  
+  if files.length < 1
+    puts "** Error: This folder #{folder} don't exists or is empty. **" 
+  else
+    index = Dir.glob('./go_versa_result*').length == 0 ? 1 : Dir.glob('./go_versa_result*').length.next
+    save_folder = "go_versa_result_#{index}"
+    
+    system("mkdir #{save_folder}") 
 
-  files.each do |file| 
-    new_file = file.split('/').last.gsub('.png', '.tiff')
-    system("convert #{file} -fuzz 0% -fill 'RGB(250,250,250)' -opaque 'RGB(255,255,255)' go_versa_tiff/#{new_file.gsub('.png', '.tiff')}") 
-    puts "#{file} has been converted. ~ format TIFF and CMYK (0,0,0,2)"
+    files.each do |file| 
+      begin 
+        image = Magick::Image.read("#{file}").first
+        image.format = 'TIFF'
+        image.fuzz = 0
+        image = image.opaque('rgba(255,255,255)','rgba(250,250,250)')
+        new_image = "#{save_folder}/#{file.split('/').last.gsub('.png', '.tiff')}"
+        image.write(new_image)
+        
+        puts "#{file} has been converted to #{new_image}"
+      rescue => e
+        puts "Any error ocurred - #{e}"
+      end
+    end
+
+    puts ""
+    puts "Thanks so much. By: Engineering Team <3"
   end
-
-  puts "Thanks so much. By: Engineering Team <3"
 end
 
 get_files
